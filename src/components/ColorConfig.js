5a7320a04b5bff1ce53a73d3c24af22461d67929
Matrix <adaly@matrixmedcom.com>
2025-07-31 10:07:05 -0400

    Update ColorConfig.js

--- src/components/ColorConfig.js
+++ src/components/ColorConfig.js
@@ -66,6 +66,8 @@ export const clientColors = {
 };
 
 export const projectColors = {
+    "2H Nurse EP": '#91a5d0',
+    "2H Nurse Conference Coverage": '#91a5d0',
     '2H Patient Podcast': '#91a5d0',
     '2H Patient Video': '#91a5d0',
     'Biorewind Video Project': '#4CB7E4', 
